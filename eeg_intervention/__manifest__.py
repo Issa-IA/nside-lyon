d00c39c7d8fa71e9ec676ab8899dabe97324f166
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2023-08-29 09:38:20 +0100

    Add files via upload

---
+++ eeg_intervention/__manifest__.py
@@ -0,0 +1,39 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Gestion RMA",
+    'summary': """
+        Gestion RMA""",
+    'version': '1.2',
+    'category': "Sales/Sales",
+    'author':
+    'shayma',
+    'website': "",
+        'depends': [
+        'base_setup',
+        'sales_team',
+        'utm',
+        'contacts',
+        'product',
+
+    ],
+    'data': [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "view/intervention_eeg_view.xml",
+        "view/etiquette_view.xml",
+        "report/Report.xml",
+        "report/Etiquettes_barcode.xml",
+        "report/bon_livraison.xml",
+        "report/box_report.xml",
+        "report/test_ok.xml",
+        "report/Fiche_suivie_par_carton.xml",
+        "report/fiche_suivie.xml",
+        "report/decimalOK.xml",
+    ],
+    'demo': [""],
+    'development_status': "Beta",
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
