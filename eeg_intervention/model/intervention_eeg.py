935382cd511bdd06c965b03b5484ea4d3b05cdee
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2023-09-11 22:11:02 +0100

    Update intervention_eeg.py

--- eeg_intervention/model/intervention_eeg.py
+++ eeg_intervention/model/intervention_eeg.py
@@ -271,6 +271,6 @@ class InterventionLineIllisible(models.Model):
     # Define fields for the quotation
     etiquette_id = fields.Many2one('model.etiquette', 'Modèle')
     task_id = fields.Many2one('project.task', 'Tâche',
-        default=lambda self.carton_id.task_id.id)
+        default=lambda self: self.carton_id.task_id.id)
     qte_illisible = fields.Integer('Qté Illisible')
     carton_id = fields.Many2one('carton.carton', 'Carton', default=lambda self: self.env['carton.carton'].search([], limit=1), index=True, copy=False)
