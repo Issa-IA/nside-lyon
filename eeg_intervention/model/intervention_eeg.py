0faff8ae50bbffa12d963cbaa2b15ec6dedca720
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2023-12-18 10:57:28 +0100

    Update intervention_eeg.py

--- eeg_intervention/model/intervention_eeg.py
+++ eeg_intervention/model/intervention_eeg.py
@@ -1,6 +1,6 @@
 import re
 import json
-from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo import api, fields, models, exceptions, SUPERUSER_ID, _
 from datetime import timedelta
 from odoo.exceptions import ValidationError, Warning
 
