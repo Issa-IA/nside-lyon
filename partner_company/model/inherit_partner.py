5f3258b1f481faa312f9e5ab61d34b80588fd734
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2024-01-15 14:35:45 +0100

    Update inherit_partner.py

--- partner_company/model/inherit_partner.py
+++ partner_company/model/inherit_partner.py
@@ -4,7 +4,13 @@ from odoo.exceptions import ValidationError
 class PartnerHerit(models.Model):
     _inherit = 'res.partner'
 
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company.id)
+    
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self._default_company_id())
+
+    @api.model
+    def _default_company_id(self):
+        # Si ref_company_ids est vide, utilisez l'entreprise actuelle, sinon, utilisez la première entreprise référencée.
+        return self.env.company.id if not self.ref_company_ids else self.ref_company_ids[0].id
 
 
 
