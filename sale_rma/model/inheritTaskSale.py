ca6125901587038132ea11fe09652ad8ee1f5fcd
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2024-02-12 16:36:17 +0100

    Update inheritTaskSale.py

--- sale_rma/model/inheritTaskSale.py
+++ sale_rma/model/inheritTaskSale.py
@@ -7,3 +7,5 @@ class inheritTask(models.Model):
 
     date_from = fields.Datetime('Date de début')
     date_to = fields.Datetime('Date de fin')
+    URL_INSIDE = "https://inside-lyon-preprod-test-10922170.dev.odoo.com"
+    base_url_1 = "https://inside-lyon-preprod-test-10922170.dev.odoo.com/web?#id={}&view_type=form&model=project.task".format(URL, id)
