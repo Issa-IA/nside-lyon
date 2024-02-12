a61f70917c8c041721a26107c4d2bc9cccddf5a0
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2024-02-12 16:48:22 +0100

    Update intervention_eeg.py

--- eeg_intervention/model/intervention_eeg.py
+++ eeg_intervention/model/intervention_eeg.py
@@ -56,6 +56,18 @@ class inheritTask(models.Model):
     transport_type = fields.Selection([('palette', 'Palette'), ('carton', 'Carton')], string='Transport',
                                       default='carton', readonly=False, compute='_compute_proprietaire_type')
     num_palette = fields.Integer(string='Quantité palette')
+    task_qr_code = fields.Binary(string="Code QR de la Tâche", compute="_compute_task_qr_code")
+
+    @api.depends('name')
+    def _compute_task_qr_code(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        for record in self:
+            task_url = "{}/web?#id={}&view_type=form&model=project.task".format(base_url, record.id)
+            qr = qrcode.make(task_url)
+            qr_img = BytesIO()
+            qr.save(qr_img)
+            record.task_qr_code = qr_img.getvalue()
+            
     task_url = fields.Char(string="URL de la Tâche", compute="_compute_task_url")
 
     @api.depends('name')
