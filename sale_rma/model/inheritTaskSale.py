57523831f2944f3a44183ca98d0775dd924850f5
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2024-11-22 15:18:35 +0100

    Update inheritTaskSale.py

--- sale_rma/model/inheritTaskSale.py
+++ sale_rma/model/inheritTaskSale.py
@@ -1,6 +1,5 @@
 from odoo import api, fields, models, SUPERUSER_ID, _
 from datetime import timedelta
-from odoo.exceptions import ValidationError,Warning
 
 class inheritTask(models.Model):
     _inherit = 'project.task'
