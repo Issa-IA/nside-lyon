2a5197abeb2ddfc2d4da1d9c04a85d6998d075a1
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2025-08-28 15:50:58 +0100

    Update inherit_stock.py

--- partner_company/model/inherit_stock.py
+++ partner_company/model/inherit_stock.py
@@ -5,3 +5,5 @@ class PartnerHerit(models.Model):
     _inherit = 'product.template'
 
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company.id)
+
+
