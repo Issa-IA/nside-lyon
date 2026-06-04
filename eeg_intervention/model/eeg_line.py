7fb18e41c193a75fccc83dc9e12ab06217370fc6
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2026-06-04 11:42:17 +0100

    Update eeg_line.py

--- eeg_intervention/model/eeg_line.py
+++ eeg_intervention/model/eeg_line.py
@@ -29,13 +29,6 @@ class ModelEtiquette(models.Model):
         ondelete='restrict')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, required=True)
 
-    @api.depends('marque_id.name')
-    def _compute_display_name(self):
-        for names in self:
-            if names.marque_id:
-                names.display_name = "[%s] %s" % (names.marque_id.name, names.name)
-            else:
-                names.display_name = names.name
 
 
 class InterventionLineEeg(models.Model):
